d05613892ed800ae409bd375b4e7f144dde10547
mohsen faraj <gmohsenfarajollahi@gmail.com>
2023-12-24 14:46:20 +0330

    updated plotting

--- genetic.py
+++ genetic.py
@@ -1,13 +1,29 @@
 import random
 class Genetic:
     bestOfGenerationsFitness = []
-
+    Avg = []
+    maxFitness = 0
+    
     def getBOG(self):
         return self.bestOfGenerationsFitness
 
+    def calcAvg(self , array):
+        sum = 0
+        for gen in array:
+            sum += self.__fitness(gen)
+        self.Avg.append(sum/len(array))
+
+    def getAvg(self):
+        return self.Avg
+    
     def clearBOG(self):
         self.bestOfGenerationsFitness.clear()
-    def __init__(self , populationCount , csrate , mrrate , elitrate , generations):
+    
+    def clearAvg(self):
+        self.Avg.clear()
+    def __init__(self , n , populationCount , csrate , mrrate , elitrate , generations):
+        self.n = n
+        self.maxFitness = ((n+1) * n / 2) - n
         self.populationCount = populationCount
         self.csrate = csrate
         self.mrrate = mrrate
@@ -15,26 +31,27 @@ class Genetic:
         self.generations = generations
 
     def __generate_individual(self):
-        return random.sample(range(8), 8)
+        return random.sample(range(self.n), self.n)
 
     def __fitness(self , individual):
         clashes = 0
-        for i in range(7):
-            for j in range(i + 1, 8):
+        for i in range(self.n - 1):
+            for j in range(i + 1, self.n):
                 if abs(i - j) == abs(individual[i] - individual[j]):
                     clashes += 1
-        return 28 - clashes  # 28 is the maximum fitness (no clashes)
+        return self.maxFitness - clashes
 
+    #TODO: crossover should return two chromosomes
     def __crossover(self , parent1, parent2):
         if (random.random() < self.csrate):
-            crossover_point = random.randint(1, 6)
+            crossover_point = random.randint(1, self.n-2)
             child = parent1[:crossover_point] + [gene for gene in parent2 if gene not in parent1[:crossover_point]]
             return child
         else:
             return parent1
 
     def __mutate(self , individual):
-        mutation_point1, mutation_point2 = random.sample(range(8), 2)
+        mutation_point1, mutation_point2 = random.sample(range(self.n), 2)
         individual[mutation_point1], individual[mutation_point2] = individual[mutation_point2], individual[mutation_point1]
 
     def solution(self):
@@ -47,8 +64,10 @@ class Genetic:
             population = sorted(population, key=lambda x: self.__fitness(x), reverse=True)
 
             self.bestOfGenerationsFitness.append(self.__fitness(population[0]))
-
-            if self.__fitness(population[0]) == 28:
+ 
+            #Calc Avg
+            self.calcAvg(population)
+            if self.__fitness(population[0]) == self.maxFitness:
                 return(f"Solution found in generation {generation + 1}: {population[0]}")
             
             #Elitism
@@ -66,4 +85,4 @@ class Genetic:
                 new_population.append(child)
 
             population = new_population
-        return (f"Best Effort: {population[0]} , fitness:{self.__fitness(population[0])}/28")
+        return (f"Best Effort: {population[0]} , fitness:{self.__fitness(population[0])}/{self.maxFitness}")
--- main.py
+++ main.py
@@ -5,10 +5,29 @@ import matplotlib.pyplot as plt
 from genetic import Genetic
 
 fig, ax = plt.subplots()
-def plot_array(array):
+maxFitness = 0
+def execute() :
+    genetic = Genetic(
+        n = int(nEntry.get()) , 
+        populationCount= int(popEntry.get()),
+        csrate= float(csoverEntry.get()),
+        mrrate= float(MREntry.get()),
+        elitrate= float(EREntry.get()),
+        generations= int(genEntry.get()))
+    solution = genetic.solution()
+    print(genetic.solution())
+    # maxFitness = genetic.maxFitness
+    Resultlbl.config(text=solution)
+    print("Avg: " , genetic.getAvg())
+    plot_array(genetic.getBOG() , genetic.getAvg())
+    genetic.clearBOG()
+    genetic.clearAvg()
+
+def plot_array(best , avg):
     ax.clear()
-    ax.bar(range(len(array)), array)
-    ax.set_ylim(24, 28)
+    ax.plot(best , 'b.-' , label="best")
+    ax.plot(avg , 'g.--' , label="average")
+    # ax.set_ylim(maxFitness, maxFitness/2)
     ax.yaxis.set_major_locator(plt.MaxNLocator(integer=True))
     ax.xaxis.set_major_locator(plt.MaxNLocator(integer=True))
     ax.set_title("Best of Generations")
@@ -16,25 +35,11 @@ def plot_array(array):
     ax.set_ylabel("Fitness")
     canvas.draw()
 
-def execute() :
-    genetic = Genetic(
-        populationCount= int(popEntry.get()),
-        csrate= float(csoverEntry.get()),
-        mrrate= float(MREntry.get()),
-        elitrate= float(EREntry.get()),
-        generations= int(genEntry.get()))
-    solution = genetic.solution()
-    Resultlbl.config(text=solution)
-    plot_array(genetic.getBOG())
-    genetic.clearBOG()
-    del genetic
-
-
 root = tk.Tk()
 root.geometry("1024x600")
 root.title("8-Queen Solver")
 
-greeting = tk.Label(root , text="8-Queen Solver" ,
+greeting = tk.Label(root , text="N-Queen Solver" ,
 font="Monospace 18 bold" ,
 )
 greeting.grid(row=0 , columnspan=2)
@@ -45,46 +50,53 @@ fg="grey"
 )
 codedBy.grid(row=1 , columnspan=2)
 
+nlbl = tk.Label(root , text="N:")
+nlbl.grid(row=2 , column=0)
+
+nEntry = tk.Entry(root)
+nEntry.insert(0, '8')
+nEntry.grid(row=2 , column=1)
+
 poplbl = tk.Label(root , text="Population")
-poplbl.grid(row= 2 , column= 0)
+poplbl.grid(row= 3 , column= 0)
 
 popEntry = tk.Entry(root)
-popEntry.insert(0, '50')
-popEntry.grid(row= 2 , column= 1)
+popEntry.insert(0, '150')
+popEntry.grid(row= 3 , column= 1)
 
 csoverlbl = tk.Label(root , text="CrossOver Rate")
-csoverlbl.grid(row= 3 , column= 0)
+csoverlbl.grid(row= 4 , column= 0)
 
 csoverEntry = tk.Entry(root)
-csoverEntry.insert(0, '0.6')
-csoverEntry.grid(row=3 , column=1)
+csoverEntry.insert(0, '0.7')
+csoverEntry.grid(row=4 , column=1)
 
 MRlbl = tk.Label(root , text="Mutation Rate")
-MRlbl.grid(row = 4 , column= 0)
+MRlbl.grid(row = 5 , column= 0)
 
 MREntry = tk.Entry(root)
-MREntry.insert(0, '0.1')
-MREntry.grid(row=4 , column=1)
+MREntry.insert(0, '0.2')
+MREntry.grid(row=5 , column=1)
 
 ERlbl = tk.Label(root , text="Elitisim Rate")
-ERlbl.grid(row=5 , column=0)
+ERlbl.grid(row=6 , column=0)
 
 EREntry = tk.Entry(root)
-EREntry.insert(0, '0.02')
-EREntry.grid(row=5 , column=1)
+EREntry.insert(0, '0.05')
+EREntry.grid(row=6 , column=1)
 
 genlbl = tk.Label(root , text="Generations")
-genlbl.grid(row=6 , column=0)
+genlbl.grid(row=7 , column=0)
 
 genEntry = tk.Entry(root)
 genEntry.insert(0, '1000')
-genEntry.grid(row=6 , column=1)
+genEntry.grid(row=7 , column=1)
 
 Execbtn = tk.Button(root , text="Execute" , command=execute)
-Execbtn.grid(row=7 , columnspan=2)
+Execbtn.grid(row=8 , columnspan=2)
 
 Resultlbl = tk.Label(root , text="Results")
-Resultlbl.grid(row=8 , columnspan=2)
+Resultlbl.grid(row=9 , columnspan=2)
 
 canvas = FigureCanvasTkAgg(figure=fig , master=root)
 canvas_widget = canvas.get_tk_widget()
